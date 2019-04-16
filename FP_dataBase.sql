58c8a02430602b59d7dac92ec71131c6b9bc83d8
Dylan Cicale <45367874+dylanc97@users.noreply.github.com>
2019-04-16 19:14:11 -0400

    Add files via upload

--- FP_dataBase.sql
+++ FP_dataBase.sql
@@ -2,26 +2,28 @@ DROP DATABASE fp_db;
 CREATE DATABASE fp_db;
 
 CREATE TABLE IF NOT EXISTS `fp_db`.`user` (
+  `name` 	 VARCHAR(20) NOT NULL,
   `userName` VARCHAR(45) NOT NULL,
-  `password` VARCHAR(45) NULL DEFAULT NULL,
-  `maxPrice` INT(11) NULL DEFAULT NULL,
+  `password` VARCHAR(45) NOT NULL,
+  `maxPrice` INT(11) NOT NULL,
+  `company`  VARCHAR(5) NOT NULL,
 PRIMARY KEY (`userName`));
 
 CREATE TABLE IF NOT EXISTS `fp_db`.`game` (
   `name` VARCHAR(45) NOT NULL,
-  `genre` VARCHAR(45) NULL DEFAULT NULL,
+  `genre` VARCHAR(45) NOT NULL,
 PRIMARY KEY (`name`));
 
 CREATE TABLE IF NOT EXISTS `fp_db`.`parts` (
   `name` VARCHAR(45) NOT NULL,
-  `category` VARCHAR(45) NULL DEFAULT NULL,
-  `price` INT(11) NULL DEFAULT NULL,
+  `category` VARCHAR(45) NOT NULL,
+  `price` INT(11) NOT NULL,
   `company` VARCHAR(45) NULL DEFAULT NULL,
   `link` VARCHAR(1000) NULL DEFAULT NULL,
 PRIMARY KEY (`Name`));
 
-INSERT INTO fp_db.user (name, userName, password, maxPrice) VALUES
-("Dylan Cicale", "dylanc97", "The", 1200);
+INSERT INTO fp_db.user (name, userName, password, maxPrice, company) VALUES
+("Dylan Cicale", "dylanc97", "The", 1200, "INTEL");
 
 INSERT INTO fp_db.game (name, genre) VALUES
 ("Fortnite", "Battle Royale"),
