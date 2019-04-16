818ed177b2b82e7062b84388b1df47cd54380704
Dylan Cicale <45367874+dylanc97@users.noreply.github.com>
2019-04-16 18:31:40 -0400

    Add files via upload

--- FP_dataBase.sql
+++ FP_dataBase.sql
@@ -9,10 +9,7 @@ PRIMARY KEY (`userName`));
 
 CREATE TABLE IF NOT EXISTS `fp_db`.`game` (
   `name` VARCHAR(45) NOT NULL,
-  `publisher` VARCHAR(45) NULL DEFAULT NULL,
-  `matchSize` INT(11) NULL DEFAULT NULL,
   `genre` VARCHAR(45) NULL DEFAULT NULL,
-  `imgLink` VARCHAR(1000) NULL DEFAULT NULL,
 PRIMARY KEY (`name`));
 
 CREATE TABLE IF NOT EXISTS `fp_db`.`parts` (
@@ -23,8 +20,21 @@ CREATE TABLE IF NOT EXISTS `fp_db`.`parts` (
   `link` VARCHAR(1000) NULL DEFAULT NULL,
 PRIMARY KEY (`Name`));
 
+INSERT INTO fp_db.user (name, userName, password, maxPrice) VALUES
+("Dylan Cicale", "dylanc97", "The", 1200);
 
-INSERT INTO fp_db.game (name, publisher, genre) VALUES
-("Fortnite", "Epic Games", "Battle Royale");
+INSERT INTO fp_db.game (name, genre) VALUES
+("Fortnite", "Battle Royale"),
+("APEX Legends", "Battle Royale"),
+("Playerunknown's Battlegrounds", "Battle Royale"),
+("Call Of Duty", "Battle Royale"),
+("Grand Theft Auto 5", "Open World"),
+("World of Warcraft", "Open World"),
+("Counter-Strike", "Standard"),
+("Overwatch", "Standard"),
+("Battlefield 5", "Standard"),
+("Rainbow 6 siege", "Standard");
 
 select * from fp_db.game;
+select * from fp_db.user;
+select * from fp_db.parts;
